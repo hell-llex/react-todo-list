54ff0a5f3e83b8c3ad1677df94b007ea08fd658a
hell-llex <demeshenko333@gmail.com>
2023-07-13 16:11:35 +0300

    feat: add base component and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.scss'
+import './App.scss';
+import ControlsBar from './components/ControlsBar/ControlsBar';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="wrapper">
+      <ControlsBar />
+      <TodoList />
+    </div>
+  );
 }
 
-export default App
+export default App;
